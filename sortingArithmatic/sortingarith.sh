5e15ad74bfd34ba49ec57e9e3eaa9c678ba8c101
atikalg <who.atik@gmail.com>
2022-03-20 19:58:42 +0530

    storing values in array

--- sortingArithmatic/sortingarith.sh
+++ sortingArithmatic/sortingarith.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
-
+dictionary -A emp
+count=0
 read -p "enter the value of a : " a
 read -p "enter the value of b : " b
 read -p "enter the value of c : " c
@@ -7,3 +8,6 @@ d=$(($a+$b*$c))
 e=$(($a*$b+$c))
 f=$(($c+$a/$b))
 g=$(($a%$b+$c))
+
+emp[((count++))]="$d $e $f $g"
+
