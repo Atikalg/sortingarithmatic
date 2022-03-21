192a51bad8e323c5a3bfd1b29132513589529f4e
atikalg <who.atik@gmail.com>
2022-03-21 14:55:54 +0530

    printing in decending array

--- sortingArithmatic/sortingarith.sh
+++ sortingArithmatic/sortingarith.sh
@@ -1,6 +1,7 @@
-#!/bin/bash -x
+##!/bin/bash -x
 dictionary -A emp
 count=0
+n=4
 read -p "enter the value of a : " a
 read -p "enter the value of b : " b
 read -p "enter the value of c : " c
@@ -9,6 +10,36 @@ e=$(($a*$b+$c))
 f=$(($c+$a/$b))
 g=$(($a%$b+$c))
 
-emp[((count++))]="$d $e $f $g"
+emp[((count++))]=$d
+emp[((count++))]=$e
+emp[((count++))]=$f
+emp[((count++))]=$g
 
 echo "${emp[@]}"
+for((j=0; j<4 ;j++))
+	do
+	i=0;
+	min=1000;
+	max=0;
+
+
+
+	for((i=0; i<4; i++))
+	do
+
+		num=$((emp[$i]))
+		if [ $num -gt $max ]
+			then
+			max=$num
+			index=$i
+		fi
+	done
+
+
+	sort[$j]=$max
+	emp[$index]=0
+
+done
+
+echo sort: ${sort[@]}
+
