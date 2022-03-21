52983e93f90b5d83ce048217c641ab5869d19de2
atikalg <who.atik@gmail.com>
2022-03-21 15:53:09 +0530

    printing array in descending order

--- sortingArithmatic/sortingarith.sh
+++ sortingArithmatic/sortingarith.sh
@@ -1,7 +1,7 @@
 ##!/bin/bash -x
 dictionary -A emp
 count=0
-n=4
+
 read -p "enter the value of a : " a
 read -p "enter the value of b : " b
 read -p "enter the value of c : " c
@@ -16,30 +16,23 @@ emp[((count++))]=$f
 emp[((count++))]=$g
 
 echo "${emp[@]}"
-for((j=0; j<4 ;j++))
-	do
-	i=0;
-	min=1000;
-	max=0;
 
+for ((i = 0; i<4; i++))
+do
 
+    for((j = 0; j<4-i; j++))
+    do
 
-	for((i=0; i<4; i++))
-	do
-
-		num=$((emp[$i]))
-		if [ $num -gt $max ]
-			then
-			max=$num
-			index=$i
-		fi
-	done
-
-
-	sort[$j]=$max
-	emp[$index]=0
-
+        if [ ${emp[j]} -gt ${emp[$((j+1))]} ]
+        then
+            # swap
+            temp=${emp[j]}
+            emp[$j]=${emp[$((j+1))]}
+            emp[$((j+1))]=$temp
+        fi
+    done
 done
+echo ${emp[@]}
 
-echo sort: ${sort[@]}
-
+rev=($(printf '%s\n' "${emp[@]}" | sort -nr))
+echo ${rev[@]}
