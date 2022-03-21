e328a700ccceb5b978f9bf1a313d27cf40a5e0ee
atikalg <who.atik@gmail.com>
2022-03-21 16:48:36 +0530

    Final submit

--- sortingArithmatic/sortingarith.sh
+++ sortingArithmatic/sortingarith.sh
@@ -1,7 +1,7 @@
-##!/bin/bash -x
+#!/bin/bash -x
 dictionary -A emp
 count=0
-n=4
+
 read -p "enter the value of a : " a
 read -p "enter the value of b : " b
 read -p "enter the value of c : " c
@@ -19,17 +19,31 @@ echo "${emp[@]}"
 
 for ((i = 0; i<4; i++))
 do
+ HEAD
     
     for((j = 0; j<4-i; j++))
     do
     
+
+
+    for((j = 0; j<4-i; j++))
+    do
+
+
         if [ ${emp[j]} -gt ${emp[$((j+1))]} ]
         then
             # swap
             temp=${emp[j]}
+
             emp[$j]=${emp[$((j+1))]}  
+
+            emp[$j]=${emp[$((j+1))]}
+
             emp[$((j+1))]=$temp
         fi
     done
 done
 echo ${emp[@]}
+
+rev=($(printf '%s\n' "${emp[@]}" | sort -nr))
+echo ${rev[@]}
